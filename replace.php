ba6a6ab7a42bf5244f9d3a0230752666ef0716c3
kamiokan <spherenov@gmail.com>
2019-08-10 16:41:52 +0900

    参照ドキュメント情報を追加

--- replace.php
+++ replace.php
@@ -1,5 +1,8 @@
 <?php
-// preg_replace と array_keys を使った一括置き換えの動作確認テスト
+// preg_replace() と array_keys() を使った一括置き換えの動作確認テスト
+// @see https://www.php.net/manual/ja/function.preg-replace.php
+// @see https://www.php.net/manual/ja/function.array-keys.php
+// @see https://www.php.net/manual/ja/reference.pcre.pattern.modifiers.php
 
 $string = "人間を人間たらしめるのは愛なのではないか。そこに愛はあるのか。それを私は問いたい。";
 $map = [
